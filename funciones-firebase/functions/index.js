b938711d3296f2d97c7aa8ac2f5d715a1c598f22
stiag0 <stiago01r@gmail.com>
2017-12-01 17:25:03 -0500

    comentado

--- funciones-firebase/functions/index.js
+++ funciones-firebase/functions/index.js
@@ -9,16 +9,18 @@ var functions = require('firebase-functions');
 exports.totalizarCarrito = functions.database
 .ref('/USUARIOS/{USUARIOSID}/carritoCompras')
 .onWrite(event =>{ 
+	// esto es para coger la infurmacion de todo el carrito de compras
 	const carritoCompra = event.data.val()
-	
 	console.log(carritoCompra.vaciar.isVerified)
-
+	// preguntamos por vaciar, para que no entre 2 veces
 	if (carritoCompra.vaciar.isVerified == 'true') {
 		console.log("se fue")
 		return
 	}
+	// aqui modificamos el valor vaciar
 	carritoCompra.vaciar.isVerified = true
 	console.log("entro  >:v")
+	// cambiamos la informacion del DataBase por el nuevo carritoCompras
 	const promise = event.data.ref.set(carritoCompra)
 	return promise
 })
